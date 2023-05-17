abdb58d77a902ac7b49ed627d8b2795aa68f6978
blake S <design.harls@icloud.com>
2023-05-17 23:30:54 +0200

    planetas añadidos en el timeline

--- src/components/Planets.vue
+++ src/components/Planets.vue
@@ -6,11 +6,12 @@ const props = defineProps({
         type: Array,
         default: () => [
 
+            { name: "Sol" },
             { name: "Mercurio" },
             { name: "Venus" },
             { name: "Tierra" },
             { name: "Marte" },
-            { name: "Júpiter" },
+            { name: "Jupiter" },
             { name: "Saturno" },
             { name: "Urano" },
             { name: "Neptuno" },
@@ -25,20 +26,13 @@ const props = defineProps({
 
 
 </script>
+
 <template>
-    <!-- <div class="planets">
-        <ul class="lista">
-            <li :class="AnimationTimeline" v-for="planet in planets" :key="planets.name">{{ planet.name }}</li>
-        </ul>
-    </div> -->
-
-
-
     <ol class="timeline">
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/sol.png" />
                 </i>
             </span>
 
@@ -47,7 +41,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/mercurio.png" />
                 </i>
             </span>
 
@@ -55,7 +49,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/venus.png" />
                 </i>
             </span>
 
@@ -63,7 +57,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/earth.png" />
                 </i>
             </span>
 
@@ -71,7 +65,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/mars.png" />
                 </i>
             </span>
 
@@ -79,7 +73,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/jupiter.png" />
                 </i>
             </span>
 
@@ -87,7 +81,7 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/saturn.png" />
                 </i>
             </span>
 
@@ -95,15 +89,15 @@ const props = defineProps({
         <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/urano.png" />
                 </i>
             </span>
 
         </li>
-        Ñ<li class="timeline-item">
+        <li class="timeline-item">
             <span class="timeline-item-icon | avatar-icon">
                 <i class="avatar">
-                    <img src="https://assets.codepen.io/285131/hat-man.png" />
+                    <img src="../assets/neptuno.png" />
                 </i>
             </span>
 
@@ -112,129 +106,111 @@ const props = defineProps({
     </ol>
 </template>
   
-  
+
 <style lang="scss" scoped>
-//  .planets{
-
-//     display: flex;
-//     gap: 2px;
-//     margin: 4px;
-
-//     .lista{
-
-//         display:flex;
-//         justify-content: center;
-//         flex-direction: column;
-
-
-//         .enume{
-//             color:rebeccapurple;
-//             width: 4px;
-//         }
-//     }
-//  }
-
-
-
-
 *,
 *:before,
 *:after {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
-
-
 
 body {
-    line-height: 1.5;
-    min-height: 100vh;
-    font-family: "Outfit", sans-serif;
-    padding-top: 20vh;
-    padding-bottom: 20vh;
+  line-height: 1.5;
+  min-height: 100vh;
+  font-family: "Outfit", sans-serif;
+  padding-top: 20vh;
+  padding-bottom: 20vh;
 }
-
-
-
-
 
 img {
-    display: block;
-    max-width: 100%;
+  display: block;
+  max-width: 100%;
 }
-
-
 
 .timeline {
-    width: 85%;
-    max-width: 700px;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    flex-direction: column;
-    padding: 32px 0 32px 32px;
-    border-left: 2px solid rgb(0, 0, 0);
-    font-size: 1.125rem;
+  width: 85%;
+  max-width: 700px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 32px 0 32px 32px;
+  border-left: 2px solid rgb(0, 0, 0);
+  font-size: 1.125rem;
 }
-
 .timeline-item {
-    display: flex;
-    gap: 24px;
+  display: flex;
+  gap: 24px;
 
-    &+* {
-        margin-top: 24px;
-    }
+  &+* {
+      margin-top: 24px;
+  }
 
-    &+.extra-space {
-        margin-top: 48px;
-    }
+  &+.extra-space {
+      margin-top: 48px;
+  }
 }
-
-
 
 .timeline-item-icon {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    margin-left: -52px;
-    flex-shrink: 0;
-    overflow: hidden;
-    box-shadow: 0 0 0 6px #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-left: -52px;
+  flex-shrink: 0;
+  overflow: hidden;
+  box-shadow: 0 0 0 6px #fff;
 
-    svg {
-        width: 20px;
-        height: 20px;
-    }
+  svg {
+      width: 20px;
+      height: 20px;
+  }
 
-    &.faded-icon {
-        background-color: var(--c-grey-100);
-        color: var(--c-grey-400);
-    }
+  &.faded-icon {
+      background-color: var(--c-grey-100);
+      color: var(--c-grey-400);
+  }
 
-    &.filled-icon {
-        background-color: var(--c-blue-500);
-        color: #fff;
-    }
+  &.filled-icon {
+      background-color: var(--c-blue-500);
+      color: #fff;
+  }
 }
-
-
-
 .avatar {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    overflow: hidden;
-    aspect-ratio: 1 / 1;
-    flex-shrink: 0;
-    width: 40px;
-    height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  overflow: hidden;
+  aspect-ratio: 1 / 1;
+  flex-shrink: 0;
+  width: 40px;
+  height: 40px;
 
-    img {
-        object-fit: cover;
-    }
+  img {
+      object-fit: cover;
+  }
 }
 </style>
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
